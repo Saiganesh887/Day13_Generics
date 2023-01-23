b2a27df0672f5d48c44188b1e0b301f3bbaae13d
Your Name <you@example.com>
2023-01-23 08:35:43 +0530

    max of three

--- src/com/bridgelabz/TestMaximum.java
+++ src/com/bridgelabz/TestMaximum.java
@@ -1,18 +1,20 @@
 package com.bridgelabz;
 
 public class TestMaximum {
-    public static<T extends Comparable<T>> T Maximum(T a,T b,T c)
+    public static<T extends Comparable<T>> T max(T a,T b,T c)
     {
-        T max = a;
+        T maxNumber = a;
 
-        if (b.compareTo(max) > 0)
-            max = b;
-        if (c.compareTo(max) > 0)
-            max = c;
+        if (b.compareTo(maxNumber) > 0)
+            maxNumber = b;
+        if (c.compareTo(maxNumber) > 0)
+            maxNumber = c;
 
-        return max;
+        return maxNumber;
     }
         public static void main(String[] args) {
-            System.out.println( Maximum(4,3,2));
+            System.out.println( max(4,3,2));
+            System.out.println(max(5.6,7.9,5.7));
+            System.out.println(max("7","9","8"));
         }
 }
